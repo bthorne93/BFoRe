585caf783d64440972f15660c04650dff9e3c53e
Ben Thorne <ben.thorne@physics.ox.ac.uk>
2017-11-14 15:27:36 -0500

    Removed prints tatemtn.

--- read_dbg.py
+++ read_dbg.py
@@ -4,10 +4,11 @@ import healpy as hp
 import sys as sys
 
 fname_read=sys.argv[1]
-
+import matplotlib as mpl
+mpl.style.use("/home/bthorne/.config/matplotlib/blt_paper.pltstyle")
 def read_debug(fname) :
     f=open(fname,"rb")
-    fsize,ipix,nside,nside_spec,n_sub,n_nu,n_pol,n_comp,n_spec_vary,n_samples,nb_samples=np.fromfile(f,dtype=np.int32,count=11)
+    fsize,ipix,_, nside,nside_spec,n_sub,n_nu,n_pol,n_comp,n_spec_vary,n_samples,nb_samples=np.fromfile(f,dtype=np.int32,count=12)
     if fsize==4 :
         print "is float"
         dt=np.float32
@@ -21,14 +22,14 @@ def read_debug(fname) :
     amin2perpix=4*np.pi*(180*60/np.pi)**2/hp.nside2npix(nside)
 
     chain=(np.fromfile(f,dtype=dt,count=n_spec_vary*(n_samples+nb_samples))).reshape(n_samples+nb_samples,n_spec_vary)
-    
+
     indices_mean=np.fromfile(f,dtype=dt,count=n_spec_vary)
     indices_covar=(np.fromfile(f,dtype=dt,count=n_spec_vary*n_spec_vary)).reshape(n_spec_vary,n_spec_vary)
     amp_mean=(np.fromfile(f,dtype=dt,count=n_comp*n_pol*n_sub)).reshape(n_side_sub,n_side_sub,n_pol,n_comp)
     amp_covar=(np.fromfile(f,dtype=dt,count=n_comp*n_comp*n_pol*n_sub)).reshape(n_side_sub,n_side_sub,n_pol,n_comp,n_comp)
     input_data=(np.fromfile(f,dtype=dt,count=n_nu*n_pol*n_sub)).reshape(n_side_sub,n_side_sub,n_pol,n_nu)
     input_noise=(np.fromfile(f,dtype=dt,count=n_nu*n_pol*n_sub)).reshape(n_side_sub,n_side_sub,n_pol,n_nu)
-    
+
     for i in np.arange(n_spec_vary) :
         plt.hist(chain[nb_samples:,i],bins=100); plt.show()
         plt.plot(chain[nb_samples:,i]); plt.xlabel('#sample',fontsize=16); plt.ylabel('$\\beta_s$',fontsize=16); plt.show()
@@ -41,7 +42,7 @@ def read_debug(fname) :
     print "Mean and rms:", indices_mean, np.sqrt(np.diag(indices_covar))
     print "Covar:"
     print indices_covar
-  
+
     print "Mean amplitudes"
     for icomp in np.arange(n_comp) :
         for ipol in np.arange(n_pol) :
